e622f92eeabc8e389f9e63c1a710c8f345d5dd6a
Teerasej Jiraphatchandej <teerasej@gmail.com>
2022-05-03 18:03:56 +0700

    first commit with simple SHA256 hash form

---
+++ android/app/src/main/kotlin/com/example/hash_sha256/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hash_sha256
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'dart:convert';
+
+import 'package:crypto/crypto.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hash SHA256',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Hash SHA256'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String hashed = '...';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: SizedBox(
+          width: 500,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'Data:',
+              ),
+              TextField(
+                onChanged: (value) {
+                  var bytes = utf8.encode(value);
+
+                  setState(() {
+                    hashed = sha256.convert(bytes).toString();
+                  });
+                },
+              ),
+              Text(
+                hashed,
+                style: Theme.of(context).textTheme.headline4,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
