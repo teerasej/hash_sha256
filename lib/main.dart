0f9a5d4207767e919056671d3fa89b6012bb7af0
Teerasej Jiraphatchandej <teerasej@gmail.com>
2022-05-04 14:14:41 +0700

    make main page as main nav

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'dart:convert';
 
-import 'package:crypto/crypto.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
+import 'package:hash_sha256/pages/hash/hash_page.dart';
+import 'package:hash_sha256/pages/nonce/nonce_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -34,87 +33,30 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String hashed = '...';
+  final tab = TabBar(tabs: <Tab>[
+    Tab(icon: Icon(Icons.arrow_forward)),
+    Tab(icon: Icon(Icons.arrow_downward)),
+    Tab(icon: Icon(Icons.arrow_back)),
+  ]);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: SizedBox(
-          width: 500,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                'Data:',
-              ),
-              TextField(
-                onChanged: (value) {
-                  var bytes = utf8.encode(value);
-
-                  setState(() {
-                    hashed = sha256.convert(bytes).toString();
-                  });
-                },
-              ),
-              Text(
-                hashed,
-                style: Theme.of(context).textTheme.headline4,
-              ),
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          bottom: TabBar(
+            tabs: [
+              Tab(text: 'Hash'),
+              Tab(text: 'Nounce'),
             ],
           ),
         ),
-      ),
-      bottomNavigationBar: BottomAppBar(
-        child: Padding(
-          padding: const EdgeInsets.all(10),
-          child: RichText(
-            text: TextSpan(
-              children: [
-                const TextSpan(
-                  text: 'Nextflow.in.th - click for more: ',
-                ),
-                TextSpan(
-                  style: const TextStyle(
-                    decoration: TextDecoration.underline,
-                    color: Colors.blue,
-                  ),
-                  recognizer: TapGestureRecognizer()
-                    ..onTap = () => {
-                          launchUrl(
-                            Uri.https(
-                              'nextflow.in.th',
-                              '/blockchain-for-developer-training-workshop',
-                            ),
-                          )
-                        },
-                  text: 'Blockchain Application Development Training',
-                ),
-                TextSpan(
-                  text: ' | ',
-                ),
-                TextSpan(
-                  style: TextStyle(
-                    decoration: TextDecoration.underline,
-                    color: Colors.blue,
-                  ),
-                  recognizer: TapGestureRecognizer()
-                    ..onTap = () => {
-                          launchUrl(
-                            Uri.https(
-                              'nextflow.in.th',
-                              'course/google-flutter-for-web-dev',
-                            ),
-                          )
-                        },
-                  text: 'Flutter Training',
-                )
-              ],
-            ),
-          ),
+        body: TabBarView(
+          children: [
+            HashPage(),
+            NoncePage(),
+          ],
         ),
       ),
     );
