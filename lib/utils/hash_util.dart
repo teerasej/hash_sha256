6fdf052aaddd40bba663ab96aed1d8866e233b43
Teerasej Jiraphatchandej <teerasej@gmail.com>
2022-05-04 18:17:29 +0700

    create mining method to find best nonce - nonce that generates hash that start with specific number of 0

--- lib/utils/hash_util.dart
+++ lib/utils/hash_util.dart
@@ -1,10 +1,44 @@
 import 'dart:convert';
 import 'package:crypto/crypto.dart';
+import 'package:hash_sha256/utils/block_hash_model.dart';
 
 class HashUtil {
   static String hashSHA264(String data, String nonce) {
     var bytes = utf8.encode(data + nonce);
 
     return sha256.convert(bytes).toString();
+  }
+
+  static Future<BlockHashModel> hashWithZeroCountCondition(
+      String data, int zeroCount,
+      {int nonce = 0}) async {
+    var count = zeroCount;
+    var validating = '';
+    var completedZeroCount = false;
+    while (!completedZeroCount) {
+      validating = HashUtil.hashSHA264(data, '$nonce');
+
+      var setOfZeroes = validating.substring(0, count).split('');
+      var zeroCount = 0;
+
+      for (var char in setOfZeroes) {
+        if (!char.contains('0')) {
+          break;
+        } else {
+          zeroCount += 1;
+        }
+      }
+
+      if (zeroCount == setOfZeroes.length) {
+        completedZeroCount = true;
+      } else {
+        nonce += 1;
+      }
+    }
+
+    return BlockHashModel(
+      validating,
+      nonce: nonce,
+    );
   }
 }
