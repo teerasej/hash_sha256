fbff4e9db29add928d5b09221f3c54a5695146a1
Teerasej Jiraphatchandej <teerasej@gmail.com>
2022-05-04 14:58:15 +0700

    make form parse data and nonce to hash

--- lib/pages/nonce/nonce_page.dart
+++ lib/pages/nonce/nonce_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hash_sha256/utils/hash_util.dart';
 
 class NoncePage extends StatefulWidget {
   NoncePage({Key? key}) : super(key: key);
@@ -8,6 +9,22 @@ class NoncePage extends StatefulWidget {
 }
 
 class _NoncePageState extends State<NoncePage> {
+  final _formKey = GlobalKey<FormState>();
+
+  String _data = '';
+  int _nonce = 0;
+  String _hashed = '...';
+
+  void doHash() {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+
+      setState(() {
+        _hashed = HashUtil.hashSHA264(_data + _nonce.toString());
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -21,6 +38,7 @@ class _NoncePageState extends State<NoncePage> {
             child: Padding(
               padding: const EdgeInsets.all(15.0),
               child: Form(
+                key: _formKey,
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
@@ -33,20 +51,38 @@ class _NoncePageState extends State<NoncePage> {
                     SizedBox(
                       height: 10,
                     ),
-                    Text('Nounce:'),
-                    TextFormField(),
-                    SizedBox(
-                      height: 10,
-                    ),
                     Text('Data:'),
                     TextFormField(
                       maxLines: 10,
+                      onChanged: (value) => doHash(),
+                      onSaved: (newValue) => _data = newValue ?? '',
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please fill the form';
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text('Nounce:'),
+                    TextFormField(
+                      onChanged: (value) => doHash(),
+                      onSaved: (newValue) =>
+                          _nonce = int.parse(newValue ?? '0'),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please fill the form';
+                        }
+                        return null;
+                      },
                     ),
                     SizedBox(
                       height: 10,
                     ),
                     Text('Hash:'),
-                    Text('...'),
+                    Text(_hashed),
                   ],
                 ),
               ),
