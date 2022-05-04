dd34867a96036f401b91461cb5194e5ef80c67e6
Teerasej Jiraphatchandej <teerasej@gmail.com>
2022-05-04 14:14:56 +0700

    create nounce page demo

---
+++ lib/pages/nonce/nonce_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NoncePage extends StatefulWidget {
+  NoncePage({Key? key}) : super(key: key);
+
+  @override
+  State<NoncePage> createState() => _NoncePageState();
+}
+
+class _NoncePageState extends State<NoncePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
